c919a283ed6732c2c48325c5c621d449e48718d5
Andreas Lundgren <andreas.lundgren@gmail.com>
2012-06-11 06:31:18 +0000

    Got node to serve out the bootstrap content

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var app = express.createServer();
 
-//Routes
+app.use(express.logger());
+
 app.use("/css", express.static(__dirname + '/client/css'));
 app.use("/js", express.static(__dirname + '/client/js'));
 app.use("/img", express.static(__dirname + '/client/img'));
-app.use("/", express.static(__dirname + '/client/html'));
+app.use('/', express.static(__dirname + '/client/html'));
 
 app.on('close', function() {
     cn.disconnect();
